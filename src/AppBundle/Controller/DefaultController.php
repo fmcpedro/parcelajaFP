cdeda8a10842f2de8e8ef8491073d779dfd210e9
Luis Miguens <luis.t.miguens@gmail.com>
2017-06-06 17:57:25 +0000

    send email

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,7 +16,85 @@ class DefaultController extends Controller
     }
     
     
+    public function emailAction(Request $request)
+    {
 
+        
+            //contact
+
+//        $name = $request->query->get('name');
+//        if (!$name):
+//            $name = $request->request->get('name');
+//        endif;
+//
+//        $email = $request->query->get('email');
+//        if (!$email):
+//            $email = $request->request->get('email');
+//        endif;
+//
+//        $subject = $request->query->get('subject');
+//        if (!$subject):
+//            $subject = $request->request->get('subject');
+//        endif;
+//
+//        $message = $request->query->get('message');
+//        if (!$message):
+//            $message = $request->request->get('message');
+//        endif;
+
+        
+        
+            // CONFIG YOUR FIELDS
+    //============================================================
+    $name =     filter_var($request->request->get("name"), FILTER_SANITIZE_STRING);
+    $email =    filter_var($request->request->get("email"), FILTER_SANITIZE_EMAIL);
+    $formMessage =  filter_var($request->request->get("message"), FILTER_SANITIZE_STRING);
+        
+        
+            // CONFIG YOUR EMAIL MESSAGE
+    //============================================================
+    $message = '<p>The following request was sent from: </p>';
+    $message .= '<p>Name: ' . $name . '</p>';
+    $message .= '<p>Email: ' . $email . '</p>';
+    $message .= '<p>Message: ' . $formMessage .'</p>';
+    
+    
+    
+    
+
+        $email_message = \Swift_Message::newInstance()
+                ->setSubject('Contact request')
+                ->setFrom($email)
+                ->setTo('geral@parcelaja.pt')
+                ->setBcc('cvalente@parcelaja.pt')
+                ->setBody($message);
+
+        //$this->get('mailer')->send($email_message);
+
+
+//        $response = array();
+//        $response["success"] = true;
+//        header('Access-Control-Allow-Origin: *');
+
+
+if(!$this->get('mailer')->send($email_message)) {
+        $data['error']['title'] = 'Message could not be sent.';
+        //$data['error']['details'] = 'Mailer Error: ' . $mail->ErrorInfo;
+        $data['error']['details'] = 'Mailer Error: ';
+       exit;
+    }
+
+    $data['success']['title'] = 'Message has been sent';
+
+    echo json_encode($data);
+        
+        
+    }
+    
+    
+    
+    
+    
     public function testeAction(Request $request)
     {
         // replace this example code with whatever you need
