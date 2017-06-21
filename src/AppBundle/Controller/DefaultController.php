ccb35b8790248692f5e95f77e8036f42cb7b8b4f
Luis Miguens <luis.t.miguens@gmail.com>
2017-06-21 11:19:36 +0000

    emails

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,71 +16,61 @@ class DefaultController extends Controller {
     public function emailAction(Request $request) {
 
 
-        //contact
-//        $name = $request->query->get('name');
-//        if (!$name):
-//            $name = $request->request->get('name');
-//        endif;
-//
-//        $email = $request->query->get('email');
-//        if (!$email):
-//            $email = $request->request->get('email');
-//        endif;
-//
-//        $subject = $request->query->get('subject');
-//        if (!$subject):
-//            $subject = $request->request->get('subject');
-//        endif;
-//
-//        $message = $request->query->get('message');
-//        if (!$message):
-//            $message = $request->request->get('message');
-//        endif;
+
         // CONFIG YOUR FIELDS
         //============================================================
         $name = filter_var($request->request->get("name"), FILTER_SANITIZE_STRING);
-        $email = filter_var($request->request->get("email"), FILTER_SANITIZE_EMAIL);
+        $emailFrom = filter_var($request->request->get("email"), FILTER_SANITIZE_EMAIL);
         $formMessage = filter_var($request->request->get("message"), FILTER_SANITIZE_STRING);
 
 
         // CONFIG YOUR EMAIL MESSAGE
         //============================================================
-        $message = '<p>The following request was sent from: </p>';
-        $message .= '<p>Name: ' . $name . '</p>';
-        $message .= '<p>Email: ' . $email . '</p>';
-        $message .= '<p>Message: ' . $formMessage . '</p>';
+//        $message = '<p>The following request was sent from: </p>';
+//        $message .= '<p>Name: ' . $name . '</p>';
+//        $message .= '<p>Email: ' . $emailFrom . '</p>';
+//        $message .= '<p>Message: ' . $formMessage . '</p>';
 
-
-
+        
+        
+        $message = "teste";
 
 
         $email_message = \Swift_Message::newInstance()
-                ->setSubject('Contact request')
-                ->setFrom($email)
-                ->setTo('geral@parcelaja.pt')
-                ->setBcc('luis.t.miguens@gmail.com')
+               ->setSubject('Contact request')
+                ->setFrom('luis.t.miguens@gmail.com')
+                ->setTo('luis.t.miguens@gmail.com')
+                ->setCc('luis.t.miguens@gmail.com')
                 ->setBody($message, 'text/html');
 
-        //$this->get('mailer')->send($email_message);
+        
+        $recipients = $this->get('mailer')->send($email_message);
+
+        
+        return $this->render('AppBundle:default:email.html.twig', []);
+
+
+
+
 //        $response = array();
 //        $response["success"] = true;
 //        header('Access-Control-Allow-Origin: *');
 
 
-        if (!$this->get('mailer')->send($email_message)) {
-            $data['error']['title'] = 'Message could not be sent.';
-            //$data['error']['details'] = 'Mailer Error: ' . $mail->ErrorInfo;
-            $data['error']['details'] = 'Mailer Error: ';
-            exit;
-        }
-
-        $data['success']['title'] = 'Message has been sent';
-
-        
-        return new \Symfony\Component\HttpFoundation\JsonResponse([
-            'success' => true,
-            'data'    => $data // Your data here
-        ]);
+//        if (!$this->get('mailer')->send($email_message)) {
+//            $data['error']['title'] = 'Message could not be sent.';
+//            //$data['error']['details'] = 'Mailer Error: ' . $mail->ErrorInfo;
+//            $data['error']['details'] = 'Mailer Error: ';
+//            exit;
+//        }
+//
+//        $data['success']['title'] = $recipients;
+//
+//        
+//        return new \Symfony\Component\HttpFoundation\JsonResponse([
+//            'success' => true,
+//            'data'    => $data // Your data here
+//        ]);
         
         //return json_encode($data);
     }
